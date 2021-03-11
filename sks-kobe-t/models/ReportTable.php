b5ca559c707dc0e311d532d01b714d61e31453ef
nishidai-itc <nishidai@e-itc.co.jp>
2021-03-11 19:58:20 +0900

    不要な項目削除

--- sks-kobe-t/models/ReportTable.php
+++ sks-kobe-t/models/ReportTable.php
@@ -12,11 +12,11 @@
             "wk_ship_in_port_time7","wk_ship_out_port_time7","wk_ship8","wk_ship_in_port_time8","wk_ship_out_port_time8","wk_ship9","wk_ship_in_port_time9","wk_ship_out_port_time9",
             "wk_in_out_start_time","wk_in_out_end_time","wk_joban_time","wk_kaban_time","wk_vp_end_time","wk_vp_kaban_time","koyo_joban_time","koyo_kaban_time","sumii_joban_time",
             "sumii_kaban_time","last_exit","last_exit1","last_exit2","yard_on_time1","yard_off_time1","yard_on_time2","yard_off_time2","depo_joban_time","depo_kaban_time","depo_num","depo_zan","sort_joban_time",
-            "sort_kaban_time","sort_num","sort_zan","cy_joban_time","cy_kaban_time","cy_num","cy_zan","exit_joban_time","exit_kaban_time","exit_num","exit_zan","vp_joban_time","vp_kaban_time","vp_num",
+            "sort_kaban_time","sort_num","sort_zan","cy_joban_time","cy_kaban_time","cy_num","cy_zan",/*"exit_joban_time","exit_kaban_time","exit_num","exit_zan",*/"vp_joban_time","vp_kaban_time","vp_num",
             "vp_zan","midday_joban_time","midday_kaban_time","midday_num","midday_zan","gate_joban_time","gate_kaban_time","gate_num","gate_zan","mbath_joban_time","mbath_kaban_time","mbath_num",
             "mbath_zan","picket_joban_time1","picket_kaban_time1","picket_num1","picket_zan1","picket_joban_time2","picket_kaban_time2","picket_num2","picket_zan2","picket_joban_time3",
-            "picket_kaban_time3","picket_num3","picket_zan3","picket_joban_time4","picket_kaban_time4","picket_num4","picket_zan4","comment","patrol_time1","patrol_time2","patrol_time3",
-            "patrol_time4","patrol_time5","patrol_time6","patrol_time7","patrol_time8","metertb1","metertb2","metertc1","metertc2","wk_staff_id1","wk_staff_id2","wk_staff_id3","wk_staff_id4",
+            "picket_kaban_time3","picket_num3","picket_zan3","picket_joban_time4","picket_kaban_time4","picket_num4","picket_zan4","comment",/*"patrol_time1","patrol_time2","patrol_time3",*/
+            /*"patrol_time4","patrol_time5","patrol_time6","patrol_time7","patrol_time8",*/"metertb1","metertb2","metertc1","metertc2","wk_staff_id1","wk_staff_id2","wk_staff_id3","wk_staff_id4",
             "wk_staff_id5","wk_staff_id6","wk_staff_id7","wk_staff_id8","wk_staff_id9","wk_staff_id10","wk_staff_id11","wk_staff_id12","wk_staff_id13","wk_staff_id14","wk_staff_id15","wk_comment"
         );
 
