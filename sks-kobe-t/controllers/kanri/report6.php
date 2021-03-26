df52d02e7ee20636927cf9c4810c0f755689f400
nishidai <nishidai@e-itc.co.jp>
2021-03-26 16:43:28 +0900

    不具合修正

--- sks-kobe-t/controllers/kanri/report6.php
+++ sks-kobe-t/controllers/kanri/report6.php
@@ -27,7 +27,7 @@
   if ($_GET["plan_date"] != "") {
     $start_date                   = $_GET["plan_date"];
   }
-  $end_date                       = date("Y-m-d");
+  $end_date                       = $start_date;
   $joban_time                     = array("08","00");
   $kaban_time                     = array("17","00");
   $wk_start_time1                 = array("07","30");
@@ -42,21 +42,12 @@
   $outsider                       = null;
 
 
-  $comment                        = "巡回点検整備その他服務中異状ありません";
+  $comment                        = "巡回点検警備その他服務中異状ありません";
   $times                          = array("1"=>array("08","00"),"2"=>array("12","00"),"3"=>array("13","00"),"4"=>array("16","30"));
-  // for ($i=1;$i<=16;$i++) {
-  //   ${"patrol_time".$i}                 = array(null,null);
-    // ${"patrol_time".$i}           = array($times[$i][0],$times[$i][1]);
-    // ${"wk_staff_id".$i}           = null;
-  // }
-  for ($i=1;$i<=9;$i++) {
+  $title                          = array("A","B","C","D");
+  for ($i=1;$i<=4;$i++) {
     ${"wk_staff_id".$i}           = null;
   }
-  // for ($i=1;$i<=2;$i++) {
-  //   ${"offwk_staff_id".$i}         = null;
-  // }
-
-
   
   // $week           = array("日", "月", "火", "水", "木", "金", "土");
   // $time           = strtotime(date($date));
@@ -70,6 +61,31 @@
     }
   }
 
+  function valChange($val) {
+    // 配列の場合（時刻等）
+    if (is_array($val)) {
+        // 値が空ならNULLで登録
+        if ($val[0] === "" && $val[1] === "") {
+            $val = null;
+        // 値が空じゃない場合
+        } elseif ($val[0] !== "" && $val[1] !== "") {
+            $val = sprintf("%02d",$val[0]).":".sprintf("%02d",$val[1]);
+        } elseif ($val[0] !== "" && $val[1] === "") {
+            $val = sprintf("%02d",$val[0]).":00";
+        } elseif ($val[0] === "" && $val[1] !== "") {
+            $val = "00:".sprintf("%02d",$val[1]);
+        }
+    // 配列じゃない場合（時刻以外）
+    } else {
+        if ($val === "" || $val === "0") {
+            $val = null;
+        } else {
+            $val = $val;
+        }
+    }
+    return $val;
+}
+
   if (isset($_POST["act"])) {
     $act        = $_POST["act"];
   }
@@ -82,6 +98,7 @@
     ********************************************************/
   $common         = new Common;         // 共通クラス
   $wkdetail       = new Wkdetail;       // 作業実施テーブルクラス
+  $wkdetail2       = new Wkdetail;
   $staff          = new Staff;          // 社員マスタクラス
   $staff2         = new Staff;
   $genba          = new Genba;
@@ -105,21 +122,7 @@
     // exit;
     foreach ($_POST as $key => $value) {
       if ($key != "act") {
-        if (is_array($value)) {
-          if ($value[0] && $value[1]) {
-            // var_dump($key,$value);
-            $report2->{"inp_".$key}               = $value[0].":".$value[1];
-          } elseif (!$value[0] && !$value[1]) {
-            $report2->{"inp_".$key}               = null;
-          }
-        } else {
-          if (!$value) {
-            // var_dump($key,$value);
-            $report2->{"inp_".$key}               = null;
-          } else {
-            $report2->{"inp_".$key}               = $value;
-          }
-        }
+        $report2->{"inp_".$key}               = valChange($value);
       }
     }
 
@@ -204,11 +207,20 @@
   }
 
   $wkdetail->inp_t_wk_genba_id      = "1";
-  $wkdetail->inp_t_wk_plan_hosoku   = "神";
+  $wkdetail->inp_t_wk_plan_hosoku_in   = "'神','5'";
   $wkdetail->inp_t_wk_plan_kbn      = "2";
   $wkdetail->inp_t_wk_plan_date     = str_replace("-","",$start_date);
   $wkdetail->inp_order              = "order by t_wk_plan_kbn,t_wk_plan_joban_time";
   $wkdetail->getWkdetail();
+
+  $wkdetail2->inp_t_wk_genba_id      = "1";
+  $wkdetail2->inp_t_wk_plan_kbn_in      = "'1','2','3'";
+  $wkdetail2->inp_t_wk_plan_date     = str_replace("-","",$start_date);
+  $wkdetail2->inp_order              = "order by t_wk_plan_kbn,t_wk_plan_joban_time";
+  $wkdetail2->getWkdetail();
+
+  $plan_kbn = array("1"=>"泊","2"=>"日","3"=>"夜");
+  $plan_ken = array("1"=>"研");
 
   // 隊員取得
   if ($wkdetail->oup_t_wk_detail_no) {
@@ -216,7 +228,7 @@
     for ($i=0;$i<count($wkdetail->oup_t_wk_detail_no);$i++) {
 
       // 勤務員の項目の隊員デフォルト表示
-      if ($cnt != 9) {
+      if ($cnt != 2) {
         $cnt = $cnt + 1;
         ${"wk_staff_id".$cnt}         = $no ? ${"wk_staff_id".$cnt} : $wkdetail->oup_t_wk_taiin_id[$i];
       }
@@ -227,6 +239,19 @@
       $staff_id = $no ? $staff_id : $wkdetail->oup_t_wk_taiin_id[0];
     }
   }
+
+  // if ($wkdetail2->oup_t_wk_detail_no) {
+  //   $cnt = 2;
+  //   for ($i=0;$i<count($wkdetail2->oup_t_wk_detail_no);$i++) {
+
+  //     // // 勤務員の項目の隊員デフォルト表示
+  //     // if ($cnt != 4) {
+  //     //   $cnt = $cnt + 1;
+  //     //   ${"wk_staff_id".$cnt}         = $no ? ${"wk_staff_id".$cnt} : $wkdetail2->oup_t_wk_taiin_id[$i];
+  //     // }
+  //   }
+  // }
+
 ?>
 <?php
     // キャリア判定（PC/スマートフォン/タブレット）
--- sks-kobe-t/views/kanri/report6_html.php
+++ sks-kobe-t/views/kanri/report6_html.php
@@ -19,168 +19,256 @@
 </head>
 
 <style>
-
 </style>
 
 <body>
   <div class="container">
-    <!-- ヘッダー -->
-    <!-- <div class="page-header">
-      <br>
-      <h2>TKUシステム</h2>
-      <h4>メニュー</h4>
-    </div> -->
-
-    <!-- ログイン者情報 -->
-    <!-- <div class="row">
-      <div class="col-12">
-        <div class="card" style="padding: 10px;">
-          <div class="card-body"></div>
-        </div>
-      </div>
-    </div>
-    <br> -->
 
     <br>
     <div class="row">
-  <div class="col-12">
-    <table class="table table-borderless">
-      <tr>
-        <td>（警備場所）</td>
-        <td>（契約先）</td>
-      </tr>
-      <tr>
-        <td>RIC-5・CFS</td>
-        <td>三菱倉庫株式会社　御中</td>
-      </tr>
+      <div class="col-12">
+        <table class="table table-borderless">
+          <tr>
+            <td><label>（警備場所）</label></td>
+            <td><label>（契約先）</label></td>
+          </tr>
+
+          <tr>
+            <td><label>RIC-C5・CFS</label></td>
+            <td><label>三菱倉庫株式会社　殿</label></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <hr>
+
+    <form action="report6.php" method="post">
+    <div class="row">
+      <div class="col-12">
+        <table class="table table-borderless">
+          <tr>
+            <td><label>（勤務時間）</label></td>
+            <td><label>天候</label></td>
+            <td><label>報告者</label></td>
+          </tr>
+
+          <tr>
+            <td>
+              <label><?php echo "自）".substr($start_date,0,4)."年".substr($start_date,5,2)."月".substr($start_date,8,2)."日　(".getWeek($start_date).")"; ?></label>
+              <input type="hidden" name="start_date" value="<?php echo $start_date; ?>">
+              <div class="time">
+                <input type="number" class="text-center" name="joban_time[0]" value="<?php echo $joban_time[0]; ?>" min="0" max="23">
+                <span class="">:</span>
+                <input type="number" class="text-center" name="joban_time[1]" value="<?php echo $joban_time[1]; ?>" min="0" max="59">
+              </div>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <label><?php echo "至）".substr($end_date,0,4)."年".substr($end_date,5,2)."月".substr($end_date,8,2)."日　(".getWeek($end_date).")"; ?></label>
+              <input type="hidden" name="end_date" value="<?php echo $end_date; ?>">
+              <div class="time">
+                <input type="number" class="text-center" name="kaban_time[0]" value="<?php echo $kaban_time[0]; ?>" min="0" max="23">
+                <span class="">:</span>
+                <input type="number" class="text-center" name="kaban_time[1]" value="<?php echo $kaban_time[1]; ?>" min="0" max="59">
+              </div>
+            </td>
+
+            <td rowspan="2">
+              <select name="weather1" id="weather1" class="">
+                <option value=""></option>
+                <?php for ($i=0;$i<count($weathers);$i++) { ?>
+                <option value="<?php echo $weathers[$i]; ?>"<?php echo $weathers[$i] == $weather1 ? "selected" : "" ; ?>><?php echo $weathers[$i]; ?></option>
+                <?php } ?>
+              </select>
+              <select name="weather2" id="weather2" class="">
+                <option value=""></option>
+                <?php for ($i=0;$i<count($weathers);$i++) { ?>
+                <option value="<?php echo $weathers[$i]; ?>"<?php echo $weathers[$i] == $weather2 ? "selected" : "" ; ?>><?php echo $weathers[$i]; ?></option>
+                <?php } ?>
+              </select>
+            </td>
+            <td rowspan="2">
+            <select name="staff_id" id="staff_id" class="w-50">
+                <option value=""></option>
+                <?php if ($wkdetail->oup_t_wk_detail_no) { ?>
+                <?php for ($i=0;$i<count($wkdetail->oup_t_wk_detail_no);$i++) { ?>
+                <option value="<?php echo $wkdetail->oup_t_wk_taiin_id[$i]; ?>"<?php echo $wkdetail->oup_t_wk_taiin_id[$i] == $staff_id ? "selected" : "" ; ?>><?php echo $staff_name[$wkdetail->oup_t_wk_taiin_id[$i]]; ?></option>
+                <?php } ?>
+                <?php } ?>
+              </select>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <hr>
+
+
+    <div class="row">
+      <div class="col-12">
+      <table class="table table-borderless">
+        <tr>
+          <td><label>（１）状況</label></td>
+        </tr>
+        <tr>
+          <td><label>i.C－５・CFSゲート立哨</label></td>
+          <td>
+            <div class="time">
+              <input type="number" class="text-center" name="wk_start_time1[0]" value="<?php echo $wk_start_time1[0]; ?>" min="0" max="23">
+              <span class="">:</span>
+              <input type="number" class="text-center" name="wk_start_time1[1]" value="<?php echo $wk_start_time1[1]; ?>" min="0" max="59">
+            </div>
+            <label for="">～</label>
+            <div class="time">
+              <input type="number" class="text-center" name="wk_end_time1[0]" value="<?php echo $wk_end_time1[0]; ?>" min="0" max="23">
+              <span class="">:</span>
+              <input type="number" class="text-center" name="wk_end_time1[1]" value="<?php echo $wk_end_time1[1]; ?>" min="0" max="59">
+            </div>
+          </td>
+        </tr>
+
+        <tr>
+        <td><label>ii.搬入出車輛</label></td>
+          <td>
+            <div class="time">
+              <input type="number" class="text-center" name="wk_start_time2[0]" value="<?php echo $wk_start_time2[0]; ?>" min="0" max="23">
+              <span class="">:</span>
+              <input type="number" class="text-center" name="wk_start_time2[1]" value="<?php echo $wk_start_time2[1]; ?>" min="0" max="59">
+            </div>
+            <label for="">～</label>
+            <div class="time">
+              <input type="number" class="text-center" name="wk_end_time2[0]" value="<?php echo $wk_end_time2[0]; ?>" min="0" max="23">
+              <span class="">:</span>
+              <input type="number" class="text-center" name="wk_end_time2[1]" value="<?php echo $wk_end_time2[1]; ?>" min="0" max="59">
+            </div>
+          </td>
+        </tr>
+
+        <tr>
+        <td><label>iii.CFSゲート解錠及び施錠</label></td>
+          <td>
+            <div class="time">
+              <input type="number" class="text-center" name="wk_start_time3[0]" value="<?php echo $wk_start_time3[0]; ?>" min="0" max="23">
+              <span class="">:</span>
+              <input type="number" class="text-center" name="wk_start_time3[1]" value="<?php echo $wk_start_time3[1]; ?>" min="0" max="59">
+            </div>
+            <label for="">～</label>
+            <div class="time">
+              <input type="number" class="text-center" name="wk_end_time3[0]" value="<?php echo $wk_end_time3[0]; ?>" min="0" max="23">
+              <span class="">:</span>
+              <input type="number" class="text-center" name="wk_end_time3[1]" value="<?php echo $wk_end_time3[1]; ?>" min="0" max="59">
+            </div>
+          </td>
+        </tr>
+
+        <tr>
+          <td><label>（２）重点</label></td>
+        </tr>
+        <tr>
+          <td><label>i. 事務所並びに倉庫の火災、盗難等警戒警備、不法侵入者の警戒監視</label></td>
+        </tr>
+        <tr>
+          <td><label>ii. 搬入車輛及び、外来者の適正誘導</label></td>
+        </tr>
+
+        <tr>
+          <td><label>（３）実施</label></td>
+        </tr>
+        <tr>
+          <td><label>i. 夜間巡回、警戒警備及び外周赤外線システムの監視</label></td>
+        </tr>
+        <tr>
+          <td><label>ii. 事務所並びに倉庫の鍵の保管・管理</label></td>
+        </tr>
+        </table>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-12">
+      <table class="table table-borderless">
+    
+
     </table>
-  </div>
-</div>
-<hr>
+      </div>
+    </div>
+    <hr>
 
-<div class="row">
-  <div class="col-12">
-    <table class="table table-borderless">
-      <tr>
-        <td>（勤務時間）</td>
-        <td>天候</td>
-        <td>担当警備士</td>
-      </tr>
-      <tr>
-        <td>
-          <?php echo "自）".$dates[0]."年".$dates[1]."月".$dates[2]."日　(".$week[$w].")"; ?>
-          <input type="number" class="text-center" value="<?php echo $joban_time[0]; ?>" min="0" max="23">
-          <span class="">:</span>
-          <input type="number" class="text-center" value="<?php echo $joban_time[1]; ?>" min="0" max="59">
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <?php echo "至）".$dates2[0]."年".$dates2[1]."月".$dates2[2]."日　(".$week[$w2].")"; ?>
-          <input type="number" class="text-center" value="<?php echo $kaban_time[0]; ?>" min="0" max="23">
-          <span class="">:</span>
-          <input type="number" class="text-center" value="<?php echo $kaban_time[1]; ?>" min="0" max="59">
-        </td>
-        <td rowspan="2">
-          <select name="" id="" class="">
-            <option value=""></option>
-            <?php for ($i=1;$i<count($weathers)+1;$i++) { ?>
-            <option value="<?php echo $i; ?>"<?php echo $i == $weather ? "selected" : "" ; ?>><?php echo $weathers[$i]; ?></option>
+    <div class="row">
+      <div class="col-12">
+        <table class="table table-borderless">
+          <tr><label>警備員</label></tr>
+          <tr>
+            <?php for ($i=1;$i<=4;$i++) { ?>
+            <td>
+              <label><?php echo $title[$i-1]; ?></label>
+              <select name="wk_staff_id<?php echo $i; ?>" id="wk_staff_id<?php echo $i; ?>" class="">
+                <option value=""></option>
+                <?php if ($i <= 2 && $wkdetail->oup_t_wk_detail_no) { ?>
+                <?php for ($j=0;$j<count($wkdetail->oup_t_wk_detail_no);$j++) { ?>
+                <option value="<?php echo $wkdetail->oup_t_wk_taiin_id[$j]; ?>"<?php echo $wkdetail->oup_t_wk_taiin_id[$j] == ${"wk_staff_id".$i} ? "selected" : "" ; ?>>
+                  <?php echo $plan_kbn[$wkdetail->oup_t_wk_plan_kbn[$j]]; ?>
+                  <?php echo $plan_ken[$wkdetail->oup_t_wk_plan_kensyu[$j]]; ?>
+                  <?php echo $staff_name[$wkdetail->oup_t_wk_taiin_id[$j]]; ?>
+                </option>
+                <?php } ?>
+                <?php } elseif ($i > 2 && $wkdetail2->oup_t_wk_detail_no) { ?>
+                <?php for ($j=0;$j<count($wkdetail2->oup_t_wk_detail_no);$j++) { ?>
+                <option value="<?php echo $wkdetail2->oup_t_wk_taiin_id[$j]; ?>"<?php echo $wkdetail2->oup_t_wk_taiin_id[$j] == ${"wk_staff_id".$i} ? "selected" : "" ; ?>>
+                  <?php echo $plan_kbn[$wkdetail2->oup_t_wk_plan_kbn[$j]]; ?>
+                  <?php echo $plan_ken[$wkdetail2->oup_t_wk_plan_kensyu[$j]]; ?>
+                  <?php echo $staff_name[$wkdetail2->oup_t_wk_taiin_id[$j]]; ?>
+                </option>
+                <?php } ?>
+                <?php } ?>
+              </select>
+            </td>
             <?php } ?>
-          </select>
-          <select name="" id="" class="">
-            <option value=""></option>
-            <?php for ($i=1;$i<count($weathers)+1;$i++) { ?>
-            <option value="<?php echo $i; ?>"<?php echo $i == $weather ? "selected" : "" ; ?>><?php echo $weathers[$i]; ?></option>
-            <?php } ?>
-          </select>
-        </td>
-        <td rowspan="2">
-          <select name="" id="" class="">
-            <option value=""></option>
-            <?php /*if ($staff2->oup_m_staff_id) { ?>
-            <?php for ($i=0;$i<count($staff2->oup_m_staff_id);$i++) { ?>
-            <option value="<?php echo $staff2->oup_m_staff_id[$i]; ?>"<?php echo $staff2->oup_m_staff_id[$i] == $staff_id ? "selected" : "" ; ?>><?php echo $staff2->oup_m_staff_name[$i]; ?></option>
-            <?php } ?>
-            <?php }*/ ?>
-          </select>
-        </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <hr>
+
+      <div class="row">
+      <div class="col-12">
+      <table class="table table-borderless">
+      <tr>
+          <td><label>（時間外）</label></td>
+        </tr>
+        <tr>
+          <td><label>人数</label><input type="text" name="offwk_count" id="" value="<?php echo $offwk_count; ?>"><label>&nbsp;部外者入門</label><input type="text" name="outsider" id="" value="<?php echo $outsider; ?>"></td>
+        </tr>
+        <!-- <tr>
+          <td><label>部外者入門</label><input type="text" name="" id="" value=""></td>
+        </tr> -->
+
+      <tr>
+          <td rowspan="5">
+            <label>（備考）</label>
+            <textarea name="comment" id="" class="w-100" cols="" rows="5" value="<?php echo $comment; ?>"><?php echo $comment; ?></textarea>
+          </td>
       </tr>
-    </table>
-  </div>
-</div>
-<hr>
-    <div class="row">
-    　<div class="col-12">１.状況 </div>
-      <div class="col-12">i.　　C－５　CFSゲート立哨　　<input type="time" class="" value="07:30">　～　<input type="time" class="" value="18:00"></div>
-      <div class="col-12">ii.　　搬入出車輛　　　　　　　<input type="time" class="" value="07:30">　～　<input type="time" class="" value="18:00"></div>
-      <div class="col-12">iii. 　CFSゲート解錠及び施錠 　　<input type="time" class="" value="07:30">　～　<input type="time" class="" value="18:00"></div>
-    </div>
-    <hr>
 
-    <div class="row">
-      <div class="col-12">２.重点</div>
-      <div class="col-12">i. 事務所並びに倉庫の火災、盗難等警戒警備、不法侵入者の警戒監視</div>
-      <div class="col-12">ii. 搬入車輛及び、外来者の適正誘導</div>
-    </div>
-    <hr>
-
-    <div class="row">
-      <div class="col-12">３.実施</div>
-      <div class="col-12">i. 夜間巡回、警戒警備及び外周赤外線システムの監視</div>
-      <div class="col-12">ii. 事務所並びに倉庫の鍵の保管・管理</div>
-    </div>
-    <hr>
-
-    <div class="row">
-    <div class="col-12">◆巡回</div>
-       <div class="col-2">
-      <input type="time" value=""><input type="time" value=""> <input type="time" value="">
-      </div>
-
-      <div class="col-2">
-      <input type="time" value=""><input type="time" value=""><input type="time" value=""> 
-      </div>
-      <div class="col-2">
-      <input type="time" value=""><input type="time" value=""> <input type="time" value="">
-      </div>
-      <div class="col-2">
-      <input type="time" value=""><input type="time" value=""> <input type="time" value="">
-      </div>
-      <div class="col-2">
-       <input type="time" value=""><input type="time" value="">
-      </div>
-      <div class="col-2">
-      <input type="time" value=""><input type="time" value="">
-      </div>
-
-    </div>
-    <hr>
-
-    <div class="row">
-      <div class="col-12">時間外　　　<input type="text" value="2名">　部外者入門　<input type="text" value="6名"></div>
-      &nbsp;
-      <div class="col-12">勤務者　　　<input type="text" value="陣野">　<input type="text" value="中根">　<input type="text" value=""></div>
-      <div class="col-12">　　　　　　<input type="text" value="">　<input type="text" value="">　<input type="text" value=""></div>
-      <div class="col-12">　　　　　　<input type="text" value="">　<input type="text" value="">　<input type="text" value=""></div>
-
-    </div>
-    <hr>
-
-    <div class="row">
-      <div class="col-1 m-auto">備<br>考</div>
-      <div class="col-11">
-        <textarea name="" id="" rows="5" class="form-control">巡回点検警備その他服務中異常ありません</textarea>
+        </table>
       </div>
     </div>
     <hr>
+
+
+    <input type="hidden" name="no" value="<?php echo $no; ?>">
+    <input type="hidden" name="act" value="">
+    </form>
+
     
     <div class="row">
       <div class="col-12 text-right">（株）新神戸セキュリティ</div>
     </div>
     <br>
 
-    <div class="row">
+    <!-- <div class="row">
       <div class="col-4">
         <button type="button" class="btn btn-warning btn-block regist" role="button">一時保存</button>
       </div>
@@ -189,6 +277,21 @@
       </div>
       <div class="col-4">
         <button type="button" class="btn btn-secondary btn-block" role="button" onclick="location.href='report_menu.php'">戻る</button>
+      </div>
+    </div> -->
+    <div class="row">
+      <div class="col-4">
+        <button type="button" class="btn btn-warning btn-block temp" role="button">一時保存</button>
+      </div>
+      <div class="col-4">
+        <button type="button" class="btn btn-success btn-block regist" role="button">完了</button>
+      </div>
+      <div class="col-4">
+        <?php if ($_SESSION["menu_flg"] == "kanri") { ?>
+        <button type="button" class="btn btn-secondary btn-block" role="button" onclick="location.href='keibihokoku.php'">戻る</button>
+        <?php } else { ?>
+        <button type="button" class="btn btn-secondary btn-block" role="button" onclick="location.href='report_menu.php'">戻る</button>
+        <?php } ?>
       </div>
     </div>
 
@@ -206,3 +309,26 @@
 
   <div class="modal-footer"></div>
 </body>
+
+</html>
+<script type="text/javascript">
+  $('hr').css('border-top','1px solid #000')
+
+  $('table td').addClass('p-0')
+
+  $('.time').addClass('d-inline-block border border-dark')
+  $('.time [type="number"]').addClass('p-0 border-0')
+  $('.time [type="number"]').css('width','45px')
+
+  $('.temp, .regist').click(function(){
+    if (!confirm('この内容で登録します。よろしいですか？')) return false
+
+    if ($(this).hasClass('temp')) {
+      $('[name="act"]').val('2')
+    } else {
+      $('[name="act"]').val('1')
+    }
+    
+    $('form').submit()
+  })
+</script>
