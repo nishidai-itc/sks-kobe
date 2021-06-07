e9378e6deb23bee8398e18c692cd218bd9e8cbcb
tsaimen <63280547+tsaimen@users.noreply.github.com>
2021-06-07 15:19:58 +0900

    KICTチェックリスト修正
    ・和暦を西暦に変更
    ・巡回者氏名　先頭１文字空白を入れる
    ・巡回時間　センタリング
    ・チェックポイントのナナメ線を細くする

--- sks-kobe-t/views/kanri/report15_pdf_html.php
+++ sks-kobe-t/views/kanri/report15_pdf_html.php
@@ -39,33 +39,37 @@ $pdf->useTemplate($pdf->importPage(1));
 
 // 和暦
 $wnen = substr($report->oup_start_date[0],0,4);
-$wnen = intval($wnen) - 2018;
-$pdf->Text(22, 29, "令和".$wnen);     // 年
+//$wnen = intval($wnen) - 2018;
+$pdf->Text(24, 29, $wnen);     // 年
 $pdf->Text(40, 29, substr($report->oup_start_date[0],5,2));     // 月
 $pdf->Text(53, 29, substr($report->oup_start_date[0],8,2));     // 日
 $pdf->Text(67, 29, "(".$weekday1.")");     // 曜日
 
-$pdf->Text(88, 34, $staffs[$report->oup_patrol_staff_id1[0]]);     // 巡回者氏名
-$pdf->Text(108, 34, $staffs[$report->oup_patrol_staff_id2[0]]);    // 巡回者氏名1
-$pdf->Text(128, 34, $staffs[$report->oup_patrol_staff_id3[0]]);     // 巡回者氏名1
-$pdf->Text(148, 34, $staffs[$report->oup_patrol_staff_id4[0]]);    // 警備員2
-$pdf->Text(168, 34, $staffs[$report->oup_patrol_staff_id5[0]]);     // 警備員2
-$pdf->Text(187, 34, $staffs[$report->oup_patrol_staff_id6[0]]);    // 警備員3
-$pdf->Text(205, 34, $staffs[$report->oup_patrol_staff_id7[0]]);     // 警備員3
-$pdf->Text(223, 34, $staffs[$report->oup_patrol_staff_id8[0]]);    // 警備員4
-$pdf->Text(242, 34, $staffs[$report->oup_patrol_staff_id9[0]]);     // 警備員4
-$pdf->Text(260, 34, $staffs[$report->oup_patrol_staff_id10[0]]);     // 警備員4
+$pdf->SetFont('kozminproregular', '', 8);// 日本語フォント
 
-$pdf->Text(88, 39, $report->oup_patrol_time1[0]);     // 巡回1
-$pdf->Text(108, 39, $report->oup_patrol_time2[0]);     // 巡回2
-$pdf->Text(128, 39, $report->oup_patrol_time3[0]);     // 巡回3
-$pdf->Text(148, 39, $report->oup_patrol_time4[0]);     // 巡回4
-$pdf->Text(168, 39, $report->oup_patrol_time5[0]);     // 巡回1
-$pdf->Text(187, 39, $report->oup_patrol_time6[0]);     // 巡回2
-$pdf->Text(205, 39, $report->oup_patrol_time7[0]);     // 巡回3
-$pdf->Text(223, 39, $report->oup_patrol_time8[0]);     // 巡回4
-$pdf->Text(242, 39, $report->oup_patrol_time9[0]);     // 巡回4
-$pdf->Text(260, 39, $report->oup_patrol_time10[0]);     // 巡回4
+$pdf->Text(92, 34, $staffs[$report->oup_patrol_staff_id1[0]]);     // 巡回者氏名
+$pdf->Text(112, 34, $staffs[$report->oup_patrol_staff_id2[0]]);    // 巡回者氏名1
+$pdf->Text(132, 34, $staffs[$report->oup_patrol_staff_id3[0]]);     // 巡回者氏名1
+$pdf->Text(152, 34, $staffs[$report->oup_patrol_staff_id4[0]]);    // 警備員2
+$pdf->Text(171, 34, $staffs[$report->oup_patrol_staff_id5[0]]);     // 警備員2
+$pdf->Text(190, 34, $staffs[$report->oup_patrol_staff_id6[0]]);    // 警備員3
+$pdf->Text(207, 34, $staffs[$report->oup_patrol_staff_id7[0]]);     // 警備員3
+$pdf->Text(226, 34, $staffs[$report->oup_patrol_staff_id8[0]]);    // 警備員4
+$pdf->Text(244, 34, $staffs[$report->oup_patrol_staff_id9[0]]);     // 警備員4
+$pdf->Text(263, 34, $staffs[$report->oup_patrol_staff_id10[0]]);     // 警備員4
+
+$pdf->SetFont('kozminproregular', '', 9);// 日本語フォント
+
+$pdf->Text(93, 39, $report->oup_patrol_time1[0]);     // 巡回1
+$pdf->Text(113, 39, $report->oup_patrol_time2[0]);     // 巡回2
+$pdf->Text(133, 39, $report->oup_patrol_time3[0]);     // 巡回3
+$pdf->Text(153, 39, $report->oup_patrol_time4[0]);     // 巡回4
+$pdf->Text(173, 39, $report->oup_patrol_time5[0]);     // 巡回1
+$pdf->Text(191, 39, $report->oup_patrol_time6[0]);     // 巡回2
+$pdf->Text(209, 39, $report->oup_patrol_time7[0]);     // 巡回3
+$pdf->Text(227, 39, $report->oup_patrol_time8[0]);     // 巡回4
+$pdf->Text(246, 39, $report->oup_patrol_time9[0]);     // 巡回4
+$pdf->Text(264, 39, $report->oup_patrol_time10[0]);     // 巡回4
 
 $pdf->Text(94, 47, $kbn[$report->oup_sensor_select1[0]]);     // 巡回1
 $pdf->Text(114, 47, $kbn[$report->oup_sensor_select2[0]]);     // 巡回2
